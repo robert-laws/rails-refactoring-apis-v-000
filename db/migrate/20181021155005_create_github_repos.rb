0936479da3f4ade1361cc3c90821ee206ca2eef8
Robert Laws <roblaws@me.com>
2018-10-21 16:14:55 +0000

    automatically backed up by learn

--- db/migrate/20181021155005_create_github_repos.rb
+++ db/migrate/20181021155005_create_github_repos.rb
@@ -2,7 +2,7 @@ class CreateGithubRepos < ActiveRecord::Migration
   def change
     create_table :github_repos do |t|
       t.string :name
-      t.string :url
+      t.string :html_url
 
       t.timestamps null: false
     end
